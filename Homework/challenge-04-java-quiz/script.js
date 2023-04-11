a77661d8e1d9ae3857a45e36f93b39b8b2f4a494
Enrique Schwanke <126434126+schwankess@users.noreply.github.com>
2023-04-10 18:17:06 -0600

    HTML-CSS-init-java

---
+++ Homework/challenge-04-java-quiz/script.js
@@ -0,0 +1,25 @@
+const startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startGame)
+const questionContainerElement = document.getElementsByClassName("question-screen")
+
+
+function startGame() {
+    console.log("Started")
+}
+
+
+
+
+function setNextQuestion(){
+
+}
+
+
+
+
+
+function selectAnwer(){
+
+
+}
